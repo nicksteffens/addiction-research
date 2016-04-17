43adeb812c7ced72cc37c8e2b9db497b67439253
Nick Steffens <nsteffens@gmail.com>
2016-04-16 20:54:13 -0500

    wip

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,6 +11,8 @@ angular.module('starter.controllers', [])
 
   // Form data for the login modal
   $scope.loginData = {};
+  $scope.account = {};
+  $scope.loggedIn = false;
 
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
@@ -29,9 +31,16 @@ angular.module('starter.controllers', [])
     $scope.modal.show();
   };
 
+  $scope.logout = function() {
+    $scope.loggedIn = false;
+    console.log('Logging Out', $scope.loggedIn);
+  }
+
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
+    $scope.loggedIn = true;
     console.log('Doing login', $scope.loginData);
+
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
